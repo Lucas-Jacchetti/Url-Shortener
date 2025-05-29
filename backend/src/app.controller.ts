c1392dc7cdbc694231a9c310aa81fd0bba377e36
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-05-29 13:59:06 -0300

    servir o front

--- backend/src/app.controller.ts
+++ backend/src/app.controller.ts
@@ -1,8 +1,14 @@
-import { Controller, Get } from '@nestjs/common';
+import { Controller, Get, Res } from '@nestjs/common';
 import { AppService } from './app.service';
+import { Response } from 'express';
+import * as path from 'path';
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @Get()
+  async url(@Res() res: Response){
+    res.sendFile(path.join(process.cwd(), 'frontend/index.html'));
+  }
 }
--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -3,9 +3,14 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { urlShortenerModule } from './UrlShortener/urlShortener.module';
 import { MongooseModule } from '@nestjs/mongoose';
+import { ServeStaticModule } from '@nestjs/serve-static';
+import { join } from 'path';
 
 @Module({
   imports: [
+    ServeStaticModule.forRoot({
+      rootPath: join(__dirname, '..', 'frontend'),
+    }),
     urlShortenerModule,
     MongooseModule.forRoot('mongodb+srv://lucasjacchetti:i80GA1LmbvYJ8WTY@cluster0.g3fqyzq.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0')
   ],
