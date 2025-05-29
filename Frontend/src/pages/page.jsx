bef1f53c05b336ae42e1439718f76d712a3041f4
Lucas Jacchetti <lucas.jacchetti@gmail.com>
2025-05-29 01:25:36 -0300

    botao pra copiar

--- Frontend/src/pages/page.jsx
+++ Frontend/src/pages/page.jsx
@@ -7,6 +7,15 @@ function MyComponent(){
   const [shortUrl, setShortUrl] = useState(null);
   const [error, setError] = useState(null);
 
+  const [copied, setCopied] = useState(false);
+
+  const handleCopy = () => {
+    navigator.clipboard.writeText(`http://localhost:3000/url/${shortUrl}`); //copia para área de transferência
+    setCopied(true); //diz que está copiado (na pratica)
+    setTimeout(() => setCopied(false), 5000);
+  };
+
+
 
   const handleSubmit = async (event) => {
     event.preventDefault(); //previne que a pagina recarregue
@@ -24,97 +33,42 @@ function MyComponent(){
 
   return (
     <>
-      <div className=' flex flex-col h-full w-auto items-center justify-center mt-50 font-bold'>
+      <div className=' flex flex-col h-full w-auto items-center justify-center mt-34 font-bold'>
         
           <h2 className='flex items-center justify-center text-4xl'>URL Shortener</h2>
           
           <form onSubmit={handleSubmit} className='flex flex-col items-center justify-center gap-8 mt-12'>
             <input
-            className='border rounded-xl p-1 pl-3 w-90 h-12'
+            className='bg-buttongray rounded-xl p-1 pl-3 w-95 h-14'
             type="url"
             placeholder="Enter URL"
             value={originalUrl}
             onChange={(e) => setOriginalUrl(e.target.value)} //a url original vai ser a que for digitada aqui
             required //campo obrigatório
             />
-            <button type="submit" className='border rounded-xl text-xl bg-buttongray w-40 h-12 cursor-pointer flex flex-row items-center justify-center gap-3'>
+            <button type="submit" className='rounded-xl text-xl bg-buttongray w-40 h-12 cursor-pointer flex flex-row items-center justify-center gap-3'>
               Shorten
               <img src={convert} alt="" className='h-8 w-8'/>
             </button>
           </form>
 
           {shortUrl && (
-            <div className='border rounded-xl p-3 flex flex-col items-center justify-center gap-2 mt-10'>
-              <p>Shortened URL: </p>
-              <a href={`http://localhost:3000/url/${shortUrl}`} target="_blank" rel="noopener noreferrer">
-                {`http://localhost:3000/url/${shortUrl}`}
-              </a>
+            <div className='flex flex-row bg-buttongray shadow-2xl shadow-black rounded-xl mt-10 h-20'>
+              <div className='flex flex-col items-center justify-center gap-2 ml-5'>
+                <p>Shortened URL: </p>
+                <a href={`http://localhost:3000/url/${shortUrl}`} target="_blank" rel="noopener noreferrer">
+                  {`http://localhost:3000/url/${shortUrl}`}
+                </a>
+              </div>
+              <div className='flex justify-center items-center ml-5 bg-inputgray rounded-br-xl rounded-tr-xl p-3 cursor-pointer'
+                  onClick={handleCopy} //quando clicar no botão, chama a função
+                  //alterna o texto entre os dois dependendo do estado (copied se for true, copy se for false)
+                > {copied ? "Copied!" : "Copy"}
+              </div>         
             </div>
           )}
         
       </div>
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-    {/* <div>
-      <h2>URL Shortener</h2>
-        <form onSubmit={handleSubmit}>
-        <input
-          type="url"
-          placeholder="Enter URL"
-          value={originalUrl}
-          onChange={(e) => setOriginalUrl(e.target.value)}
-          required
-        />
-        <button type="submit">Shorten URL</button>
-      </form>
-
-      {shortenedUrl && (
-        <div>
-          <p>Shortened URL:</p>
-          <a href={`http://localhost:3000/url/${shortenedUrl}`} target="_blank" rel="noopener noreferrer">
-            {`http://localhost:3000/url/${shortenedUrl}`}
-          </a>
-        </div>
-      )}
-
-    </div> */}
     </>
   );
 };
